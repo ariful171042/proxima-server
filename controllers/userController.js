ebbb96b1033100eb72f0a39dcb99f04026829080
ariful171042 <arif171042@gmail.com>
2023-03-07 01:09:50 +0600

    bcript signup login done

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,14 +1,21 @@
 const User = require("../models/userModal");
 
 //Login user
-
 const loginUser = async (req, res) => {
   res.json({ message: "login" });
 };
 
 //singUp user
 const signupUser = async (req, res) => {
-  res.json({ message: "signup" });
+  const { email, password } = req.body;
+
+  try {
+    const user = await User.signup(email, password);
+
+    res.status(200).json(user);
+  } catch (err) {
+    res.status(400).json({ error: err.message });
+  }
 };
 
 module.exports = {
