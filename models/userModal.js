45bf5f1c3ae28e5dcea12af54109abc0cda9e8a6
ariful171042 <arif171042@gmail.com>
2023-03-07 07:39:34 +0600

    login logic added

--- models/userModal.js
+++ models/userModal.js
@@ -50,4 +50,25 @@ userSchema.statics.signup = async function (email, password) {
   return user;
 };
 
+userSchema.statics.login = async function (email, password) {
+  //validation
+  if (!email || !password) {
+    throw Eroor("All fields must be filled");
+  }
+
+  const user = await this.findOne({ email });
+
+  if (!user) {
+    throw Error("Incorrect email");
+  }
+
+  const match = await bcrypt.compare(password, user.password);
+
+  if (!match) {
+    throw Error("Incorrect Password");
+  }
+
+  return user;
+};
+
 module.exports = mongoose.model("User", userSchema);
